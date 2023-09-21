79140a2197efb545b8835cc7b1e95bd4469c84b1
JadynAi <936745006@qq.com>
2023-09-21 21:51:36 +0800

    change UI 2.0
    optimize UI to 2.0

--- scripts/ui.py
+++ scripts/ui.py
@@ -14,47 +14,12 @@ def on_ui_tabs():
             with gr.Column(variant='panel'):
 
                 with gr.Row():
-                    with gr.Column(elem_id="ebs_settings"):
-                        project_dir = gr.Textbox(label='Project directory', lines=1)
-                        original_movie_path = gr.Textbox(label='Original Movie Path', lines=1)
-                        with gr.Accordion("Stage1 setting",open=False):
-                            key_add_last_frame = gr.Checkbox(label="Add last frame to keyframes", value=True)
-                            selected_frame_scale_tab = gr.State(value=0)
-                            with gr.Tabs(elem_id="frame_width_height",default=1):
-                                with gr.Tab("Frame resize by size",elem_id='frame_wh_1') as frame_size:
-                                    frame_width = gr.Number(value=-1, label="Frame Width", precision=0, interactive=True)
-                                    frame_height = gr.Number(value=-1, label="Frame Height", precision=0, interactive=True)
-                                with gr.Tab("Frame resize by scale",elem_id='frame_wh_2') as frame_scale:
-                                    frame_wh_scale = gr.Slider(minimum=0.1, maximum=2.0, step=0.1, label='Width and height scaling', value=1.0)
-
-                            frame_size.select(fn=lambda: 0, inputs=[], outputs=[selected_frame_scale_tab])
-                            frame_scale.select(fn=lambda: 1, inputs=[], outputs=[selected_frame_scale_tab])
-                        with gr.Accordion("Stage4 setting(Optional)",open=False):
-                            auto_scale = gr.Checkbox(label="Auto scale,If Auto Scale is open, click generate button will scale video_key/video_key_output/video_frame/video_mask all files to original video size", value=True)
-                            scale_dir = gr.Textbox(label='Need to scale directory', lines=1)
-                            scale_selected_frame_scale_tab = gr.State(value=0)
-                            with gr.Tabs(elem_id="scale_frame_width_height",default=1):
-                                with gr.Tab("Frame resize by size",elem_id='scale_frame_wh_1') as scale_frame_size:
-                                    scale_frame_width = gr.Number(value=-1, label="Frame Width", precision=0, interactive=True)
-                                    scale_frame_height = gr.Number(value=-1, label="Frame Height", precision=0, interactive=True)
-                                with gr.Tab("Frame resize by scale",elem_id='scale_frame_wh_2') as scale_frame_scale:
-                                    scale_frame_wh_scale = gr.Slider(minimum=0.1, maximum=2.0, step=0.1, label='Width and height scaling', value=1.0)
-
-                            scale_frame_size.select(fn=lambda: 0, inputs=[], outputs=[selected_frame_scale_tab])
-                            scale_frame_scale.select(fn=lambda: 1, inputs=[], outputs=[selected_frame_scale_tab])
-                        with gr.Accordion("Stage7 setting",open=False):
-                            output_fps = gr.Number(value=-1, label="Output Video FPS", precision=0, interactive=True)
-                            blend_rate = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, label='Crossfade blend rate', value=1.0)
-                            export_type = gr.Dropdown(choices=["mp4","webm","gif","rawvideo"], value="mp4" ,label="Export type")
-                                    
-
-                    with gr.Column(variant='panel'):
+                    with gr.Column(variant='manual'):
                         with gr.Column(scale=1):
                             with gr.Group():
                                 debug_info = gr.HTML(elem_id="ebs_info_area", value=".")
 
                             with gr.Column(scale=2):
-                                stage_index = gr.Radio(label='Process Stage', choices=["stage 1","stage 1.1","stage 2","stage 3","stage 4","stage 5","stage 6","stage 7"], value="stage 1", type="index")
                                 gr.HTML(value="<p style='margin-bottom: 0.7em'>\
                                                 The process of creating a video can be divided into the following stages.<br>\
                                                 (Stage 2,3,5 only show a guide and do nothing actual processing.)<br><br>\
@@ -86,18 +51,57 @@ def on_ui_tabs():
                                                     Concatenate each frame while crossfading.<br>\
                                                     Composite audio files extracted from the original video onto the concatenated video.<br><br>\
                                                 </p>")
+                                with gr.Group():
+                                    html_info = gr.HTML()
                             
-                            with gr.Row():
-                                generate_btn = gr.Button('Generate', elem_id="ebs_generate_btn", variant='primary')
-                            
-                            with gr.Group():
-                                html_info = gr.HTML()
+                    with gr.Column(elem_id="process stage"):
+                        with gr.Tabs(elem_id="stage step",default=0):
+                            with gr.Tab("Stage 1",elem_id='stage_1') as stage1:
+                                project_dir = gr.Textbox(label='Project directory', lines=1)
+                                original_movie_path = gr.Textbox(label='Original Movie Path', lines=1)
+                                key_add_last_frame = gr.Checkbox(label="Add last frame to keyframes", value=True)
+                                selected_frame_scale_tab = gr.State(value=0)
+                                with gr.Tabs(elem_id="frame_width_height",default=1):
+                                    with gr.Tab("Frame resize by size",elem_id='frame_wh_1') as frame_size:
+                                        frame_width = gr.Number(value=-1, label="Frame Width", precision=0, interactive=True)
+                                        frame_height = gr.Number(value=-1, label="Frame Height", precision=0, interactive=True)
+                                    with gr.Tab("Frame resize by scale",elem_id='frame_wh_2') as frame_scale:
+                                        frame_wh_scale = gr.Slider(minimum=0.1, maximum=2.0, step=0.1, label='Width and height scaling', value=1.0)
+                                frame_size.select(fn=lambda: 0, inputs=[], outputs=[selected_frame_scale_tab])
+                                frame_scale.select(fn=lambda: 1, inputs=[], outputs=[selected_frame_scale_tab])
+
+                            with gr.Tab("Stage 2",elem_id='stage_2') as stage2:
+                                key_add_last_frame = gr.Checkbox(label="Add last frame to keyframes", value=True)
+                            with gr.Tab("Stage 3",elem_id='stage_3') as stage3:
+                                key_add_last_frame = gr.Checkbox(label="Add last frame to keyframes", value=True)
+                            with gr.Tab("Stage 4",elem_id='stage_4') as stage4:
+                                auto_scale = gr.Checkbox(label="Auto scale,If Auto Scale is open, click generate button will scale video_key/video_key_output/video_frame/video_mask all files to original video size", value=True)
+                                scale_dir = gr.Textbox(label='Need to scale directory', lines=1)
+                                scale_selected_frame_scale_tab = gr.State(value=0)
+                                with gr.Tabs(elem_id="scale_frame_width_height",default=1):
+                                    with gr.Tab("Frame resize by size",elem_id='scale_frame_wh_1') as scale_frame_size:
+                                        scale_frame_width = gr.Number(value=-1, label="Frame Width", precision=0, interactive=True)
+                                        scale_frame_height = gr.Number(value=-1, label="Frame Height", precision=0, interactive=True)
+                                    with gr.Tab("Frame resize by scale",elem_id='scale_frame_wh_2') as scale_frame_scale:
+                                        scale_frame_wh_scale = gr.Slider(minimum=0.1, maximum=2.0, step=0.1, label='Width and height scaling', value=1.0)
+
+                                scale_frame_size.select(fn=lambda: 0, inputs=[], outputs=[selected_frame_scale_tab])
+                                scale_frame_scale.select(fn=lambda: 1, inputs=[], outputs=[selected_frame_scale_tab])
+
+                            with gr.Tab("Stage 5",elem_id='stage_5') as stage5:
+                                key_add_last_frame = gr.Checkbox(label="Add last frame to keyframes", value=True)
+                            with gr.Tab("Stage 6",elem_id='stage_6') as stage6:
+                                key_add_last_frame = gr.Checkbox(label="Add last frame to keyframes", value=True)
+                            with gr.Tab("Stage 7",elem_id='stage_7') as stage7:
+                                output_fps = gr.Number(value=-1, label="Output Video FPS", precision=0, interactive=True)
+                                blend_rate = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, label='Crossfade blend rate', value=1.0)
+                                export_type = gr.Dropdown(choices=["mp4","webm","gif","rawvideo"], value="mp4" ,label="Export type")
 
 
             ebs_args = dict(
                 fn=wrap_gradio_gpu_call(ebsynth_utility_process),
                 inputs=[
-                    stage_index,
+                    0,
 
                     project_dir,
                     original_movie_path,
