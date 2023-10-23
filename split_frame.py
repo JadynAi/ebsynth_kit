79e30efa4f2c0ccd1bc682458c0f93eaa3bf69bd
JadynAi <936745006@qq.com>
2023-10-23 21:37:36 +0800

    Create split_frame.py

---
+++ split_frame.py
@@ -0,0 +1,80 @@
+import os
+import shutil
+
+video_frame_path = 'video_frame'  
+video_key_output_path = 'video_key_output'
+
+output_path = 'img2img_frame'
+if not os.path.exists(output_path):
+    os.mkdir(output_path)
+
+key_frames = sorted(os.listdir(video_key_output_path))
+key_frame_ids = [int(name.split('.')[0]) for name in key_frames]
+
+first_frame = os.listdir(video_frame_path)[0]
+ext = first_frame.split('.')[-1]
+if ext not in ['jpg', 'png', 'webp']:
+    raise ValueError('Unsupported image format')
+
+max_key_frame_id = max(key_frame_ids)
+frame_files = sorted(os.listdir(video_frame_path))
+max_frame_id = int(frame_files[-1].split('.')[0])
+
+for i in range(len(key_frame_ids) - 1):
+    frame_name = 'output-{:05d}'.format(key_frame_ids[i])
+    frame_path = os.path.join(output_path, frame_name)
+    os.mkdir(frame_path)
+    
+    frame_dir = os.path.join(frame_path, 'frame')
+    os.mkdir(frame_dir)
+    
+    ref_dir = os.path.join(frame_path, 'reference')
+    os.mkdir(ref_dir)
+
+    start_id = key_frame_ids[i] + 1
+    end_id = key_frame_ids[i+1]
+    for id in range(start_id, end_id):
+        frame_file = '{:05d}.{}'.format(id, ext)
+        src_frame = os.path.join(video_frame_path, frame_file)
+        dst_frame = os.path.join(frame_dir, frame_file)
+        shutil.copy(src_frame, dst_frame)
+
+        if id <= (start_id + end_id) // 2:
+            ref_file = '{:05d}.{}'.format(key_frame_ids[i], ext)
+        else:
+            ref_file = '{:05d}.{}'.format(key_frame_ids[i+1], ext)
+        src_ref = os.path.join(video_key_output_path, ref_file)
+        dst_ref = os.path.join(ref_dir, frame_file)
+        shutil.copy(src_ref, dst_ref)
+
+    key_start_ref = os.path.join(video_key_output_path, '{:05d}.{}'.format(key_frame_ids[i], ext))
+    key_end_ref = os.path.join(video_key_output_path, '{:05d}.{}'.format(end_id, ext))
+    key_start_src = os.path.join(frame_path, '{:05d}.{}'.format(key_frame_ids[i], ext))
+    key_end_src = os.path.join(frame_path, '{:05d}.{}'.format(end_id, ext))
+    shutil.copy(key_start_ref, key_start_src)
+    shutil.copy(key_end_ref, key_end_src)
+
+
+if max_key_frame_id < max_frame_id:
+    last_frame_name = 'output-{}'.format(max_key_frame_id)
+    last_frame_path = os.path.join(output_path, last_frame_name)
+    os.mkdir(last_frame_path)
+
+    frame_dir = os.path.join(last_frame_path, 'frame')
+    os.mkdir(frame_dir)
+
+    ref_dir = os.path.join(last_frame_path, 'reference')
+    os.mkdir(ref_dir)
+
+    for id in range(max_key_frame_id+1, max_frame_id+1):
+        frame_file = '{:05d}.{}'.format(id, ext)
+        src_frame = os.path.join(video_frame_path, frame_file)
+        dst_frame = os.path.join(frame_dir, frame_file)
+        shutil.copy(src_frame, dst_frame)
+        
+        ref_file = '{:05d}.{}'.format(max_key_frame_id, ext)
+        src_ref = os.path.join(video_key_output_path, ref_file)
+        dst_ref = os.path.join(ref_dir, frame_file)
+        shutil.copy(src_ref, dst_ref)
+
+print('Done!')
